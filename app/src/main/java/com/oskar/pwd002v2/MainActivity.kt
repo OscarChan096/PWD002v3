09dd18a8ea4d18042404ade59c1063da989fefb3
Oscar Chan <oscarchan96@outlook.com>
2025-01-30 01:45:00 -0600

    PRE-ALPHA

--- app/src/main/java/com/oskar/pwd002v2/MainActivity.kt
+++ app/src/main/java/com/oskar/pwd002v2/MainActivity.kt
@@ -5,66 +5,30 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.gson.GsonBuilder
-import com.oskar.pwd002v2.controller.DataPassAdapter
 import com.oskar.pwd002v2.controller.pwdViewModel
 import com.oskar.pwd002v2.obj.dataPass
-import com.oskar.pwd002v2.retrofit.ApiService
 import com.oskar.pwd002v2.ui.theme.PWD002V2Theme
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
 
-    var dataPwd: List<dataPass>? = null
-
-    /*val retrofit = Retrofit.Builder()
-        .baseUrl("https://jsonplaceholder.typicode.com/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()*/
-    val gson = GsonBuilder()
-        .registerTypeAdapter(dataPass::class.java, DataPassAdapter())
-        .create()
-    val retrofit = Retrofit.Builder()
-        .baseUrl("http://192.168.1.119:5000/api/pwd/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    val apiService = retrofit.create(ApiService::class.java)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*Log.d("onCreate", "Inicio del onCreate")
-        lifecycleScope.launch {
-            try {
-                dataPwd = apiService.getPWDbyId("activision")
-                Log.d("mostrar datos obtenidos", "Post: ${dataPwd}")
-            } catch (e: Exception) {
-                Log.e("ERROR", "No se obtuvo datos: ${e.message}")
-            }
-        }*/
         enableEdgeToEdge()
         setContent {
             PWD002V2Theme {
@@ -100,12 +64,18 @@ fun Greeting(dataPwd: List<dataPass>?) {
 }
 @Composable
 fun ItemRow(pwd: dataPass){
-    Column (modifier = Modifier.padding(5.dp)){
-        Text(text = "ID > "+pwd.ID.toString())
-        Text(text = "Date > "+pwd.FECHMODIF)
-        Text(text = "TITLE > "+pwd.TITLE)
-        Text(text = "USER > "+pwd.USERNAME)
-        Text(text = "Password > "+pwd.USERPASSWORD)
+    Card (
+        modifier = Modifier.padding(5.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Black,
+            contentColor = Color.White),
+        elevation = CardDefaults.elevatedCardElevation()
+    ){
+            Text(text = "ID > "+pwd.ID.toString(), modifier = Modifier.padding(10.dp, 5.dp, 10.dp, 5.dp))
+            Text(text = "Date > "+pwd.FECHMODIF, modifier = Modifier.padding(10.dp, 5.dp, 10.dp, 5.dp))
+            Text(text = "TITLE > "+pwd.TITLE, modifier = Modifier.padding(10.dp, 5.dp, 10.dp, 5.dp))
+            Text(text = "USER > "+pwd.USERNAME, modifier = Modifier.padding(10.dp, 5.dp, 10.dp, 5.dp))
+            Text(text = "Password > "+pwd.USERPASSWORD, modifier = Modifier.padding(10.dp, 5.dp, 10.dp, 5.dp))
     }
 }
 
@@ -113,6 +83,6 @@ fun ItemRow(pwd: dataPass){
 @Composable
 fun GreetingPreview() {
     PWD002V2Theme {
-        Greeting("Android")
+        GreetingScreen()
     }
 }*/
--- app/src/main/java/com/oskar/pwd002v2/ui/theme/Theme.kt
+++ app/src/main/java/com/oskar/pwd002v2/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.oskar.pwd002v2.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,28 +8,28 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = Color(0xFF000000),
+    onBackground = Color(0xFF000000)
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
+    tertiary = Pink40,
+    background = Color(0xFF000000),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
+    onBackground = Color(0xFF000000),
     onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
